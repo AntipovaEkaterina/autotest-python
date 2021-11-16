4cd4655b6efd958ae50c3baf824e580b10e682ed
NightCatrin <ektant9@gmail.com>
2021-11-16 17:13:45 +0700

    Open the browser

---
+++ taskOne.py
@@ -0,0 +1,22 @@
+# Сценарий 1 - открытие страницы.
+# 1.	Открыть браузер.
+# 2.	Очистить кэш браузера
+# 3.	Перейти на http://tensor.ru/
+# Собрать максимально возможное количество метрик, характеризующих скорость открытия страницы (например, события жизненного цикла страницы https://www.w3.org/TR/navigation-timing/)
+
+import pychrome
+from pychrome import tab
+import subprocess
+import time
+
+subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir=remote-profile")
+time.sleep(5)
+
+browser = pychrome.Browser(url='http://127.0.0.1:9222')
+
+tab = browser.new_tab()
+time.sleep(2)
+tab.start()
+
+tab.stop()
+browser.close_tab(tab)   

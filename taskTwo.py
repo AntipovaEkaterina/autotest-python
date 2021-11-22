f5a1a25bb9a4ab65e571e9ea1891f2d9e75d46ea
NightCatrin <ektant9@gmail.com>
2021-11-22 11:24:20 +0700

    finished taskTwo.py

--- taskTwo.py
+++ taskTwo.py
@@ -8,6 +8,7 @@
 6.	Кликнуть на первый элемент в списке 
 """
 
+import json
 import pychrome
 from pychrome import tab
 import subprocess
@@ -38,6 +39,8 @@ def findField(selector):
     input_field_box_model = get_click_coords(node_id=input_log)
     mouseClick(tab, input_field_box_model[0], input_field_box_model[1])
 
+metrics = "JSON.stringify(window.performance.timing);"
+
 subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir=remote-profile")
 time.sleep(5)
 
@@ -45,21 +48,40 @@ browser = pychrome.Browser(url='http://127.0.0.1:9222')
 tab = browser.new_tab()
 
 tab.start()
-tab.call_method("Network.enable")
-tab.call_method("Network.clearBrowserCache")
+
+tab.Network.enable()
+tab.Network.clearBrowserCache()
 tab.call_method("Page.navigate", url="https://online.sbis.ru/auth/?ret=%2Fauth", _timeout=5) 
 tab.wait(5)
 
-input_elem_demo = findField(selector=SEARCH_DEMO)
-tab.wait(5)
-input_elem_item_doc = findField(selector=SEARCH_ITEM_DOC)
+findField(selector=SEARCH_DEMO)
 tab.wait(10)
-input_elem_navig_panel = findField(selector=SEARCH_NAVIG_PANEL)
-tab.wait(5)
-input_elem_sub_menu = findField(selector=SEARCH_SUBMENU)
-tab.wait(7)
-input_elem_cont = findField(selector=SEARCH_CONTAINER)
-tab.wait(5)
+findField(selector=SEARCH_ITEM_DOC)
+tab.wait(10)
+findField(selector=SEARCH_NAVIG_PANEL)
+tab.wait(10)
+findField(selector=SEARCH_SUBMENU)
+tab.wait(10)
+findField(selector=SEARCH_CONTAINER)
+tab.wait(10)
+
+tab.Runtime.enable() 
+scriptId = tab.Runtime.compileScript(expression=metrics, sourceURL="https://online.saby.ru/page/inflow", persistScript=True)
+timing_data = json.loads(tab.Runtime.runScript(scriptId=scriptId["scriptId"])["result"]["value"])
+
+tab.wait(2)
+timings = {}
+timings["severConnectTime"] = timing_data["connectEnd"] - timing_data["connectStart"]
+timings["pageLoadTime"] = timing_data["loadEventEnd"] - timing_data["navigationStart"]
+timings["gettingAByte"] = timing_data["responseEnd"] - timing_data["requestStart"]
+timings["renderTime"] = timing_data["domComplete"] - timing_data["domLoading"]
+timings["domainLookup"] = timing_data["domainLookupEnd"] - timing_data["domainLookupStart"]
+timings["interactive"] = timing_data["domInteractive"] - timing_data["domLoading"]
+timings["contentLoadedEvent"] = timing_data["domContentLoadedEventEnd"] - timing_data["domContentLoadedEventStart"]
+timings["processingToInteractive"] = timing_data["domInteractive"] - timing_data["domLoading"]
+
+print(timings)
+
 time.sleep(5)
 
 tab.stop()
